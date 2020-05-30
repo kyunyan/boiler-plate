a32b271540f59c005f3ba00fb62acf5fe075c6bf
geonwan <psog03081@naver.com>
2020-05-30 09:04:38 +0900

    회원 가입 기능

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser");
+const {User} = require("./models/User");
+
+// bodyParser 가 클라이언트에서 오는정보를 서버에서 분석해서 가져온다
+// application/x-www-form-ureledcoded 로 된것을 분석해서 가져올수 있게 해준다.
+app.use(bodyParser.urlencoded({extended : true}));
+
+// application/json 타입으로 된것을 분석해서 가져올수 있게 해준다.
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose")
 mongoose.connect("mongodb+srv://geonwan:rjsdhks12@boilerplate-slc6j.mongodb.net/test?retryWrites=true&w=majority", {
@@ -14,6 +23,23 @@ mongoose.connect("mongodb+srv://geonwan:rjsdhks12@boilerplate-slc6j.mongodb.net/
   .catch(err => console.log(err));
 
 
-app.get('/', (req, res) => res.send('Hello World! ~ 안뇽!!'))
+app.get('/', (req, res) => res.send('Hello World! ~ 안뇽!!'));
+
+app.post("/register" , (req, res) => { debugger;
+  //  회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  // save 를 하면 user정보가 save를 통해 model 에 저장
+   // 몽고디비에서 오는 메소드
+  user.save((err, doc) => {
+    if(err) return res.json({success : false ,err});
+    return res.status(200).json({
+      success : true
+    })
+  }); 
+
+
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
